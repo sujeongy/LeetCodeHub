2dd6709e1dedf634bb42fe1528f942da6648d40f
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2024-03-22 14:05:32 +0900

    Time: 57 ms (73.82%), Space: 51.8 MB (31.68%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.ts
@@ -0,0 +1,18 @@
+function finalValueAfterOperations(operations: string[]): number {
+    let result = 0;
+    for(let i=0;i<operations.length;i++) {
+        switch (operations[i]) {
+            case '++X':
+            case 'X++':
+                result++;
+                break;
+            case '--X':
+            case 'X--':
+                result--;
+                break;
+            default:
+                console.log(`Sorry`);
+        }
+    }
+    return result;
+};
