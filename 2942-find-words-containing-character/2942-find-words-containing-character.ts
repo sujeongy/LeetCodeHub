873bf6b21bac44349314c8828cba36e29bec9f92
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2024-03-28 09:57:16 +0900

    Time: 92 ms (7.66%), Space: 54.2 MB (16.67%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.ts
+++ 2942-find-words-containing-character/2942-find-words-containing-character.ts
@@ -1,7 +1,7 @@
 function findWordsContaining(words: string[], x: string): number[] {
     let result = [];
     for(let i=0;i<words.length;i++) {
-        if(words[i].includes(x)){
+        if(words[i].indexOf(x) !== -1){
             result.push(i);
         }
     }
