172889db138bba88d2059457cc7eab0e84c2f1e4
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2024-04-02 09:52:02 +0900

    Time: 63 ms (55.87%), Space: 52.4 MB (74.15%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -1,9 +1,4 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    let result = [];
     const maxNum = Math.max(...candies);
-    for(let i=0;i<candies.length;i++) {
-        const num = candies[i] + extraCandies;
-        result.push(num >= maxNum);
-    }
-    return result;
+    return candies.map((candy) => candy+extraCandies >= maxNum);
 };
