dba5ba4ad79be42fdac1da58ea848c45e147cfc1
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2024-04-03 12:56:07 +0900

    Time: 54 ms (98.66%), Space: 52 MB (92.41%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.ts
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.ts
@@ -1,15 +1,8 @@
 function smallerNumbersThanCurrent(nums: number[]): number[] {
-  let result = [];
-  nums.forEach((ele, idx) => {
-    const newArr = [...nums];
-    newArr.splice(idx, 1);
-    let count = 0;
-    newArr.forEach((ele2) => {
-        if(ele > ele2) {
-            count++;
-        }
+    let result = [];
+    const sortedNums = [...nums].sort((a, b) => a - b);
+    nums.forEach((ele) => {
+        result.push(sortedNums.indexOf(ele));
     })
-    result[idx] = count;
-  })  
-  return result;
+    return result;
 };
