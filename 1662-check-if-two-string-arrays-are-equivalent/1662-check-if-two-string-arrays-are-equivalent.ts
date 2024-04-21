3a3352d90c2af2fa1d255829beecc154a0e9895e
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2024-04-21 19:39:58 +0900

    Time: 54 ms (71.20%), Space: 51.1 MB (91.14%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.ts
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.ts
@@ -1,5 +1,5 @@
 function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
-    const str1 = word1.join('');
-    const str2 = word2.join('');
+    const str1 = word1.length > 1 ? word1.join('') : word1[0];
+    const str2 = word2.length > 1 ? word2.join('') : word2[0];
     return str1 === str2
 };
