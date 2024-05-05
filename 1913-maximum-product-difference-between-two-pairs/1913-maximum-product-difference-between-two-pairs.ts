62ada5ec62ccefbb133a9d94a4a8bff1a88fca8f
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2024-05-05 09:19:47 +0900

    Time: 58 ms (96.39%), Space: 54.4 MB (9.64%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.ts
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.ts
@@ -1,5 +1,5 @@
 function maxProductDifference(nums: number[]): number {
-    const sortedNums = nums.sort((a,b) => a - b);
+    const sortedNums = [...nums].sort((a,b) => a - b);
     const len = nums.length;
     return (sortedNums[len-2] * sortedNums[len-1]) - (sortedNums[0] * sortedNums[1]);
 };
