8e2343ac35743a977e1ce55bf9dbaa378e53f97f
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2025-06-22 10:36:49 +0900

    Time: 2 ms (50.53%), Space: 56.2 MB (75.53%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -1,4 +1,4 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    const intersection = new Set(nums1.filter(x => nums2.includes(x)));
-    return [...intersection];  
+    const arr = nums1.filter(x => nums2.includes(x));
+    return [...new Set(arr)];  
 };
