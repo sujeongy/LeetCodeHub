b9d035dc0d1b9d2e1dd77b1e1a77e1f15ee1ed1b
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2025-06-22 10:29:47 +0900

    Time: 2 ms (50.53%), Space: 55.3 MB (94.95%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.ts
@@ -1,15 +1,4 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
-    const mapped = new Map();
-    nums1.forEach((num) => {
-        if(nums2.includes(num)) {
-            mapped.set(num, (mapped.get(num) ?? 0) + 1);
-        }
-    })
-    const iterator = mapped.keys();
-    let result = [];
-    for(let i=0;i<mapped.size;i++) {
-        const key = iterator.next().value;
-        result.push(key);
-    }
-    return result;
+    const intersection = new Set(nums1.filter(x => nums2.includes(x)));
+    return [...intersection];  
 };
