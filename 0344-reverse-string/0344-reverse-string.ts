21c9fcd23697378f49ed0788932e0010b39353b9
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2025-06-23 09:31:14 +0900

    Time: 0 ms (100%), Space: 62.4 MB (43.71%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.ts
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -5,6 +5,9 @@ function reverseString(s: string[]): void {
     const strLen = s.length;
     const len = Math.floor(strLen / 2);
     for(let i=0;i<len;i++) {
-        [s[i], s[strLen - i - 1]] = [s[strLen - i - 1], s[i]];
+        const temp = s[i];
+        const rpl = s[strLen - i - 1];
+        s[i] = rpl;
+        s[strLen - i - 1] = temp;
     }
 };
