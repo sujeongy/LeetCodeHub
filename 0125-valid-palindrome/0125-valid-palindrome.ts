b7893fcd25f3a3994837b6cca2c52a684af9094f
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2025-06-24 09:54:37 +0900

    Time: 2 ms (92.63%), Space: 58.3 MB (75.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,10 +1,12 @@
 function isPalindrome(s: string): boolean {
     if(!s.trim()) return true;
-    const str = s.toLowerCase();
-    const regexp = /[a-z0-9]/gi;
-    const matchesArray = str.match(regexp);
-    const val1 = matchesArray?.join('');
-    const reversedMatchesArray = matchesArray?.reverse();
-    const val2 = reversedMatchesArray?.join('');
-    return val1 == val2;
+    const regexp = /[^a-z0-9]/gi;
+    const newStr = s.toLowerCase().replace(regexp, '');
+    const len = newStr.length;
+    for(let i=0;i<len;i++) {
+        if(newStr[i] !== newStr[len - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
