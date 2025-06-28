a4ad3f3f9a2ebc1d7fe42dd1533df7f208ee146f
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2025-06-28 11:34:05 +0900

    Time: 2 ms (78.96%), Space: 69.6 MB (51.91%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,16 +1,18 @@
 function findMaxAverage(nums: number[], k: number): number {
-    let count = 0;
-    let currentSum = 0;
-    let maxSum = 0;
-    for(let i=0;i<nums.length;i++) {
-        currentSum += nums[i];
-        if(count < k) {
-            maxSum = currentSum;
-            count++;
-        } else {
-            currentSum -= nums[i - k];
-            maxSum = Math.max(maxSum, currentSum);
-        }
+    let windowSum = 0;
+    
+    // 첫 k개 sum 초기화
+    for (let i = 0; i < k; i++) {
+        windowSum += nums[i];
     }
+
+    let maxSum = windowSum;
+
+    // 슬라이딩 윈도우 이동
+    for (let i = k; i < nums.length; i++) {
+        windowSum = windowSum - nums[i - k] + nums[i];
+        maxSum = Math.max(maxSum, windowSum);
+    }
+
     return maxSum / k;
-};
+}
