ee682b8930b52387187749f0a899cb9072706757
Sujeong Krystal Yoo <sujeongy.dev@gmail.com>
2025-06-28 10:56:52 +0900

    Time: 2 ms (72.27%), Space: 64.1 MB (86.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.ts
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -1,13 +1,9 @@
 function maxSubArray(nums: number[]): number {
-  let maxSum = nums[0];
-  let currentSum = nums[0];
-
-  for (let i = 1; i < nums.length; i++) {
-    // 현재 원소 하나로 새로 시작할지, 기존 연속합에 더할지 선택
-    currentSum = Math.max(nums[i], currentSum + nums[i]);
-    // 최대값 갱신
-    maxSum = Math.max(maxSum, currentSum);
-  }
-
-  return maxSum;
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+    for(let i=1;i<nums.length;i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum;
 }
